369ab381769300968238cb9270c7be7c87aac64c
PALAMETI REDDY LAKSHMI MANOJ <131252726+manoj020304@users.noreply.github.com>
2025-01-28 22:45:14 +0530

    Add files via upload

---
+++ selectingROI.py
@@ -0,0 +1,60 @@
+import cv2
+import pickle
+import cvzone
+import numpy as np
+
+width, height = 107, 48
+
+try:
+    with open('parkingSlotPosition', 'rb') as f:
+        posList = pickle.load(f)
+except:
+    posList = []
+
+# Mouse click event handler
+def mouseClick(events, x, y, flags, params):
+    global posList
+    if events == cv2.EVENT_LBUTTONDOWN:
+        posList.append((x, y))
+    if events == cv2.EVENT_RBUTTONDOWN:
+        for i, pos in enumerate(posList):
+            x1, y1 = pos
+            if x1 < x < x1 + width and y1 < y < y1 + height:
+                posList.pop(i)
+    with open('parkingSlotPosition', 'wb') as f:
+        pickle.dump(posList, f)
+
+cap = cv2.VideoCapture('carParkingInput.mp4')
+
+while True:
+    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+    success, img = cap.read()
+    print()
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (3, 3), 1)
+    imgThreshold = cv2.adaptiveThreshold(imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 25, 16)
+    imgMedian = cv2.medianBlur(imgThreshold, 5)
+    kernel = np.ones((3, 3), np.uint8)
+    imgDilate = cv2.dilate(imgMedian, kernel, iterations=1)
+    
+    spaceCounter = 0
+    for pos in posList:
+        x, y = pos
+        imgCrop = imgDilate[y:y + height, x:x + width]
+        count = cv2.countNonZero(imgCrop)
+        if count < 900:
+            color = (0, 255, 0)
+            thickness = 5
+            spaceCounter += 1
+        else:
+            color = (0, 0, 255)
+            thickness = 2
+        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), color, thickness)
+    
+    cvzone.putTextRect(img, f'Free: {spaceCounter} / {len(posList)}', (100, 50), scale=3,
+                       thickness=5, offset=20, colorR=(0, 200, 0))
+    
+    cv2.imshow("Image", img)
+    cv2.setMouseCallback("Image", mouseClick)
+    cv2.waitKey(1)
